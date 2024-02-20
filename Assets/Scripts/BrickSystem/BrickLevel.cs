9a4e14a38124e42cb02a37705aaef2eea55cdac3
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2024-02-20 08:43:58 -0800

    level weditor works

---
+++ Assets/Scripts/BrickSystem/BrickLevel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "BrickLevel")]
+public class BrickLevel : ScriptableObject
+{
+    public string[] levelData;
+
+    // B - Brick 
+    // I - InvBrick
+}
--- Assets/Scripts/BrickSystem/BrickSpawner.cs
+++ Assets/Scripts/BrickSystem/BrickSpawner.cs
@@ -8,18 +8,90 @@ public class BrickSpawner : MonoBehaviour
     // Grid parameters
     // 
 
+    [Header("Grid Data")]
     [SerializeField] private GameObject brick;
-    [SerializeField] private GameObject inVBrick;
+    [SerializeField] private GameObject invBrick;
+    [SerializeField] private BrickLevel brickLevel;
+
+    [Header("Grid Size")]
+    [SerializeField] private int row = 70;
+    [SerializeField] private int col = 40;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        BuildLevel();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void BuildLevel()
+    {
+        // Builds level
+        string[] levelData = brickLevel.levelData;
+
+        bool rowExact = false;
+        bool colExact = false;
+
+        int arrCount = 0;
+
+        if (levelData.Length == row)
+        {
+            rowExact = true;
+        }
+
+        for (int i = 0; i < row; i++)
+        {
+            if (rowExact || i == 0)
+            {
+                arrCount = i;
+            }
+            else
+            {
+                arrCount = i % levelData.Length;
+            }
+
+            string currRow = levelData[arrCount];
+
+            if (currRow.Length == col)
+            {
+                colExact = true;
+            }
+            else
+            {
+                colExact = false;
+            }
+
+            int strCount = 0;
+
+            for (int j = 0; j < col; j++)
+            {
+                if (colExact || j == 0)
+                {
+                    strCount = j;
+                }
+                else
+                {
+                    strCount = j % currRow.Length;
+                }
+
+                Vector3 spawnPos = new Vector3(j - (col / 2) + 0.5f, i, 0);
+
+                if (currRow[strCount] == 'B')
+                {
+                    Instantiate(brick, spawnPos, Quaternion.identity);
+                }
+                else if (currRow[strCount] == 'I')
+                {
+                    Instantiate(invBrick, spawnPos, Quaternion.identity);
+                }
+            }
+        }
+
         
     }
 }
